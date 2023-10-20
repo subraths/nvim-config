8d9b87fed90ca3464be76980c41e4af958411d85
Subrath S <subrath@epesign.com>
2023-10-20 12:30:50 -0400

    update

--- lua/keymaps.lua
+++ lua/keymaps.lua
@@ -7,3 +7,10 @@ local builtin = require('telescope.builtin')
 
 map('n', '<leader>ff', builtin.find_files, {})
 map('n', '<leader><leader>', builtin.live_grep, {})
+
+map("n", "<leader>xx", function() require("trouble").toggle() end)
+map("n", "<leader>xw", function() require("trouble").toggle("workspace_diagnostics") end)
+map("n", "<leader>xd", function() require("trouble").toggle("document_diagnostics") end)
+map("n", "<leader>xq", function() require("trouble").toggle("quickfix") end)
+map("n", "<leader>xl", function() require("trouble").toggle("loclist") end)
+map("n", "gR", function() require("trouble").toggle("lsp_references") end)
--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -1,16 +1,15 @@
 return {
   -- the colorscheme should be available when starting neovim
-
-{
-	"navarasu/onedark.nvim",
-	lazy = false,
-	priority = 1000,
-	opts = { style = 'darker'},
-},
+  {
+    "navarasu/onedark.nvim",
+    lazy = false,
+    priority = 1000,
+    opts = { style = "darker" },
+  },
 
   -- I have a separate config.mappings file where I require which-key.
   -- With lazy the plugin will be automatically loaded when it is required somewhere
-  { "folke/which-key.nvim", lazy = true },
+  { "folke/which-key.nvim",        lazy = true },
 
   {
     "nvim-neorg/neorg",
@@ -36,15 +35,13 @@ return {
 
   "neovim/nvim-lspconfig",
 
-
-
   -- if some code requires a module from an unloaded plugin, it will be automatically loaded.
   -- So for api plugins like devicons, we can always set lazy=true
   { "nvim-tree/nvim-web-devicons", lazy = true },
 
   -- you can use the VeryLazy event for things that can
   -- load later and are not important for the initial UI
-  { "stevearc/dressing.nvim", event = "VeryLazy" },
+  { "stevearc/dressing.nvim",      event = "VeryLazy" },
 
   {
     "Wansmer/treesj",
@@ -65,7 +62,7 @@ return {
   -- { dir = "~/projects/secret.nvim" },
 
   -- you can use a custom url to fetch a plugin
- -- { url = "git@github.com:folke/noice.nvim.git" },
+  -- { url = "git@github.com:folke/noice.nvim.git" },
 
   -- local plugins can also be configure with the dev option.
   -- This will use {config.dev.path}/noice.nvim/ instead of fetching it from Github
@@ -76,56 +73,57 @@ return {
     "nvim-telescope/telescope.nvim",
   },
   {
-    'windwp/nvim-autopairs',
+    "windwp/nvim-autopairs",
     event = "InsertEnter",
-    opts = {} -- this is equalent to setup({}) function
-},
-{
-	"mhartington/formatter.nvim",
-},
-	"hrsh7th/cmp-nvim-lsp",
-	"hrsh7th/cmp-buffer",
-	"hrsh7th/cmp-path",
-	"hrsh7th/cmp-cmdline",
-	{"hrsh7th/nvim-cmp"},
-	"L3MON4D3/LuaSnip",
-	"saadparwaiz1/cmp_luasnip",
-	{
-		"williamboman/mason.nvim",
-		opts = {
-			    ui = {
+    opts = {}, -- this is equalent to setup({}) function
+  },
+  {
+    "mhartington/formatter.nvim",
+  },
+  "hrsh7th/cmp-nvim-lsp",
+  "hrsh7th/cmp-buffer",
+  "hrsh7th/cmp-path",
+  "hrsh7th/cmp-cmdline",
+  { "hrsh7th/nvim-cmp" },
+  "L3MON4D3/LuaSnip",
+  "saadparwaiz1/cmp_luasnip",
+  {
+    "williamboman/mason.nvim",
+    opts = {
+      ui = {
         icons = {
-            package_installed = "✓",
-            package_pending = "➜",
-            package_uninstalled = "✗"
-        }
-    }
-		},
-	},
-	    {
-		    "williamboman/mason-lspconfig.nvim",
-opts = {},
+          package_installed = "✓",
+          package_pending = "➜",
+          package_uninstalled = "✗",
+        },
+      },
     },
-	{
-  "pmizio/typescript-tools.nvim",
-  dependencies = { "nvim-lua/plenary.nvim", "neovim/nvim-lspconfig" },
-  opts = {},
-},
+  },
+  {
+    "williamboman/mason-lspconfig.nvim",
+    opts = {},
+  },
+  {
+    "pmizio/typescript-tools.nvim",
+    dependencies = { "nvim-lua/plenary.nvim", "neovim/nvim-lspconfig" },
+    opts = {},
+  },
 
---dap
-	"mfussenegger/nvim-dap",
-	{
-		"rcarriga/nvim-dap-ui",
-		opts = {},
-	},
-	{ "folke/neodev.nvim",
-	opts = {
-		library = {
-			plugins = { "nvim-dap-ui" },
-			types = true
-		}
-	}
-},
+  --dap
+  "mfussenegger/nvim-dap",
+  {
+    "rcarriga/nvim-dap-ui",
+    opts = {},
+  },
+  {
+    "folke/neodev.nvim",
+    opts = {
+      library = {
+        plugins = { "nvim-dap-ui" },
+        types = true,
+      },
+    },
+  },
   {
     "nvim-neo-tree/neo-tree.nvim",
     branch = "v3.x",
@@ -134,23 +132,28 @@ opts = {},
       "nvim-tree/nvim-web-devicons", -- not strictly required, but recommended
       "MunifTanjim/nui.nvim",
     },
-      keys = {
-	    {
-		    "<leader>e",
-		    function ()
-		    	require('neo-tree.command').execute({ toggle = true })
-		    end,
-	    }
-    }
-},
-{'akinsho/bufferline.nvim', version = "*",
-opts = {
-	options = {
-		offsets = {
-			filetype = "Neotree",
-			text = "File Explorer",
-		}
-	},
-},
-dependencies = 'nvim-tree/nvim-web-devicons'},
+    keys = {
+      {
+        "<leader>e",
+        function()
+          require("neo-tree.command").execute({ toggle = true })
+        end,
+      },
+    },
+  },
+  {
+    "akinsho/bufferline.nvim",
+    version = "*",
+    dependencies = "nvim-tree/nvim-web-devicons",
+    opts = {},
+  },
+  {
+    "folke/trouble.nvim",
+    dependencies = { "nvim-tree/nvim-web-devicons" },
+    opts = {},
+  },
+  {
+    "mhartington/formatter.nvim",
+    opts = {}
+  },
 }
