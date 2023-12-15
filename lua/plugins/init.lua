5ec2d85447126098974f193c6b36d08fb15c7912
Subrath <subraths@epesign.com>
2023-12-15 09:33:35 +0530

    add nvim cmp config

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -52,5 +52,11 @@ return {
 			vim.keymap.set("n", "<leader>xl", function() require("trouble").toggle("loclist") end)
 			vim.keymap.set("n", "gR", function() require("trouble").toggle("lsp_references") end)
 		end
-	}
+	},
+	{ "lukas-reineke/indent-blankline.nvim", main = "ibl", opts = {} },
+	{ "lewis6991/gitsigns.nvim", opts = {} },
+	{
+		"j-hui/fidget.nvim",
+		opts = {},
+	},
 }
