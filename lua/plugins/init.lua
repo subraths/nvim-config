a91c9b5126de1c06a07d817720087acdfbb4241d
Subrath <subraths@epesign.com>
2023-12-16 18:30:31 +0530

    update: add autopairs for function and methods

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -39,7 +39,12 @@ return {
 	{
     'windwp/nvim-autopairs',
     event = "InsertEnter",
-    opts = {} -- this is equalent to setup({}) function
+		config = function()
+			local cmp_autopairs = require('nvim-autopairs.completion.cmp')
+			local cmp = require('cmp')
+			cmp.event:on('confirm_done', cmp_autopairs.on_confirm_done())
+			require("nvim-autopairs").setup()
+		end
 	},
 	{
 		"folke/trouble.nvim",
