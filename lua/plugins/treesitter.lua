4bf05f233999236908251b02851dbe9e85b9f762
Subrath <subraths@epesign.com>
2023-12-16 21:09:07 +0530

    feature: add incremental selection using treesitter

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -1,14 +1,23 @@
 return {
-	"nvim-treesitter/nvim-treesitter", build = ":TSUpdate",
-	config = function ()
-		local config	= require("nvim-treesitter.configs")
+	"nvim-treesitter/nvim-treesitter",
+	build = ":TSUpdate",
+	config = function()
+		local config = require("nvim-treesitter.configs")
 		config.setup({
-			ensure_installed = {"lua", "tsx", "javascript", "typescript"},
+			ensure_installed = { "lua", "tsx", "javascript", "typescript" },
 			highlight = { enable = true },
 			indent = { enable = true },
 			autotag = {
 				enable = true,
-			}
+			},
+			incremental_selection = {
+				enable = true,
+				keymaps = {
+					init_selection = "<C-space>", -- set to `false` to disable one of the mappings
+					node_incremental = "<C-space>",
+					node_decremental = "<bs>",
+				},
+			},
 		})
-	end
+	end,
 }
