e7461b0dd73cfe0232db73676c73c3c6c0a2f16b
Subrath <subraths@epesign.com>
2023-12-16 18:54:39 +0530

    update: update format on save

--- lua/plugins/formatter.lua
+++ lua/plugins/formatter.lua
@@ -1,21 +1,15 @@
 return {
 	{
 		"stevearc/conform.nvim",
-		config = function()
-			local opts = {
-				formatters_by_ft = {
-					lua = { "stylua" },
-					javascript = { { "prettier", "prettierd" } },
-					typescript = { { "prettier", "prettierd" } },
-				},
-			}
-			require("conform").setup(opts)
-			vim.api.nvim_create_autocmd("BufWritePre", {
-				pattern = "*",
-				callback = function(args)
-					require("conform").format({ bufnr = args.buf })
-				end,
-			})
-		end,
+		opts = {
+			formatters_by_ft = {
+				lua = { "stylua" },
+				javascript = { { "prettier", "prettierd" } },
+				typescript = { { "prettier", "prettierd" } },
+			},
+			format_on_save = {
+				lsp_fallback = true,
+			},
+		},
 	},
 }
