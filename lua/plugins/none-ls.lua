151577af1bde18d96a4c16d3be53d910c6bddbbd
Subrath S <subrath@epesign.com>
2024-03-03 05:41:10 +0530

    feat(formatter): add rustfmt

--- lua/plugins/none-ls.lua
+++ lua/plugins/none-ls.lua
@@ -1,30 +1,31 @@
 return {
-  "nvimtools/none-ls.nvim",
-  config = function()
-    local null_ls = require("null-ls")
-    null_ls.setup({
-      sources = {
-        null_ls.builtins.formatting.stylua,
-        null_ls.builtins.formatting.prettierd,
-        null_ls.builtins.formatting.black,
-        null_ls.builtins.formatting.trim_newlines,
-        null_ls.builtins.formatting.trim_whitespace,
-        null_ls.builtins.diagnostics.eslint_d,
-      },
-    })
+	"nvimtools/none-ls.nvim",
+	config = function()
+		local null_ls = require("null-ls")
+		null_ls.setup({
+			sources = {
+				null_ls.builtins.formatting.stylua,
+				null_ls.builtins.formatting.prettierd,
+				null_ls.builtins.formatting.black,
+				null_ls.builtins.formatting.trim_newlines,
+				null_ls.builtins.formatting.trim_whitespace,
+				null_ls.builtins.diagnostics.eslint_d,
+				null_ls.builtins.diagnostics.rustfmt,
+			},
+		})
 
-    vim.keymap.set("n", "gf", vim.lsp.buf.format, {})
+		vim.keymap.set("n", "gf", vim.lsp.buf.format, {})
 
-    vim.api.nvim_create_autocmd("BufWritePre", {
-      pattern = "*",
-      callback = function()
-        vim.lsp.buf.format({
-          bufnr = bufnr,
-          filter = function(client)
-            return client.name == "null-ls"
-          end,
-        })
-      end,
-    })
-  end,
+		vim.api.nvim_create_autocmd("BufWritePre", {
+			pattern = "*",
+			callback = function()
+				vim.lsp.buf.format({
+					bufnr = bufnr,
+					filter = function(client)
+						return client.name == "null-ls"
+					end,
+				})
+			end,
+		})
+	end,
 }
