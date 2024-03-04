1799f10cde715ebf7068ddf4bdad31ea54644eea
Subrath S <subrath@epesign.com>
2024-03-04 16:46:15 +0530

    trash

---
+++ lua/plugins/lsp-zero.lua
@@ -0,0 +1,42 @@
+return {
+	{
+		"VonHeikemen/lsp-zero.nvim",
+		branch = "v3.x",
+		config = function()
+			local lsp_zero = require("lsp-zero")
+			lsp_zero.extend_lspconfig()
+			lsp_zero.on_attach(function(client, bufnr)
+				lsp_zero.default_keymaps({ buffer = bufnr })
+				lsp_zero.buffer_autoformat()
+
+				if client.supports_method("textDocument/formatting") then
+					require("lsp-format").on_attach(client)
+				end
+			end)
+
+			lsp_zero.set_sign_icons({
+				error = "✘",
+				warn = "▲",
+				hint = "⚑",
+				info = "»",
+			})
+
+			require("mason").setup({})
+			require("mason-lspconfig").setup({
+				ensure_installed = {},
+				handlers = {
+					lsp_zero.default_setup,
+				},
+			})
+		end,
+	},
+	{ "neovim/nvim-lspconfig" },
+	{ "hrsh7th/cmp-nvim-lsp" },
+	{ "hrsh7th/nvim-cmp" },
+	{ "L3MON4D3/LuaSnip" },
+	{ "williamboman/mason.nvim" },
+	{ "williamboman/mason-lspconfig" },
+	{ "lukas-reineke/lsp-format.nvim" },
+	{ "hrsh7th/cmp-nvim-lua" },
+	{ "onsails/lspkind.nvim" },
+}
